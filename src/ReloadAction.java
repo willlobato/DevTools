a613b86a74a63aa8c44709eb9e0bbf5179b4eceb
willlobato <will.lobato@gmail.com>
2017-10-03 15:30:54 -0300

    plugin melhorado

---
+++ src/ReloadAction.java
@@ -0,0 +1,101 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.twelvemonkeys.util.LinkedMap;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class ReloadAction extends AnAction {
+
+    private final static String PROFILE_PATH = "C:/devtools/var/was_liberty_profile/servers/mxlocal201711/";
+    private final static String DIR_APPS = "/apps";
+
+    /** FileNotificationMBean name */
+    private final static String MBEAN_FILE_NOTIFICATION = "WebSphere:service=com.ibm.ws.kernel.filemonitor.FileNotificationMBean";
+    /** FileNotificationMBean method notifyFileChanges signature */
+    private final static String[] MBEAN_FILE_NOTIFICATION_NOTIFYFILECHANGES_SIGNATURE = new String[] {
+            Collection.class.getName(), Collection.class.getName(), Collection.class.getName() };
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+
+        JMXConnector jmxConnector = null;
+        try {
+
+            File fileApps = new File(PROFILE_PATH + DIR_APPS);
+            File[] apps = fileApps.listFiles();
+            String[] appsChoose = new String[apps.length];
+            LinkedMap<String, File> appsMap = new LinkedMap<>();
+            for (int i=0; i<apps.length; i++) {
+                appsMap.put(apps[i].getName(), apps[i]);
+                appsChoose[i] = apps[i].getName();
+            }
+
+            final String app = (String) JOptionPane.showInputDialog(null,
+                    "Choose de application",
+                    "Application",
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    appsChoose,
+                    appsChoose[0]);
+
+
+            if(app != null) {
+                String pathUrl = PROFILE_PATH + "/logs/state/com.ibm.ws.jmx.local.address";
+                List<String> lines = Files.readAllLines(Paths.get(pathUrl));
+                String url = lines.get(0);
+
+                JMXServiceURL serviceUrl = new JMXServiceURL(url);
+                jmxConnector = JMXConnectorFactory.connect(serviceUrl, null);
+
+                MBeanServerConnection mbean = jmxConnector.getMBeanServerConnection();
+
+                // Invoke FileNotificationMBean
+                ObjectName fileMonitorMBeanName = new ObjectName(MBEAN_FILE_NOTIFICATION);
+                if (mbean.isRegistered(fileMonitorMBeanName)) {
+                    // Create a list of absolute paths of each file to be checked
+                    new File(appsMap.get(app).getAbsolutePath()).setLastModified(new Date().getTime());
+                    List<String> modifiedFilePaths = new ArrayList<>();
+                    modifiedFilePaths.add(appsMap.get(app).getAbsolutePath());
+
+                    // Set MBean method notifyFileChanges parameters (createdFiles, modifiedFiles, deletedFiles)
+                    Object[] params = new Object[]{null, modifiedFilePaths, null};
+
+                    // Invoke FileNotificationMBean method notifyFileChanges
+                    mbean.invoke(fileMonitorMBeanName, "notifyFileChanges", params,
+                            MBEAN_FILE_NOTIFICATION_NOTIFYFILECHANGES_SIGNATURE);
+                } else {
+                    Messages.showMessageDialog(project,
+                            "MBean invoke request failed " + MBEAN_FILE_NOTIFICATION + " is not registered.",
+                            "Error",
+                            Messages.getErrorIcon());
+                }
+            }
+        } catch (Exception ex) {
+            Messages.showMessageDialog(project, ex.getMessage(), "Error", Messages.getErrorIcon());
+        } finally {
+            try {
+                if(jmxConnector != null) {
+                    jmxConnector.close();
+                }
+            } catch (IOException e) {
+                Messages.showMessageDialog(project, e.getMessage(), "Error", Messages.getErrorIcon());
+            }
+        }
+    }
+
+}
